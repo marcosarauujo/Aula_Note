adc4044fa98060aa22fc7d61096d1f23bb187ad8
Marcos <mvra0802@gmail.com>
2026-09-24 16:27:10 -0300

    commit inicial

---
+++ src/main/java/com/marcos/aulanote/AulanoteApplication.java
@@ -0,0 +1,13 @@
+package com.marcos.aulanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulanoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulanoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcos/aulanote/AulanoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.aulanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulanoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
